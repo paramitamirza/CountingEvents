b3e0ada0a8877bb784ea69428e2cd92c0c647d8a
paramitamirza <paramita135@gmail.com>
2018-01-04 08:51:58 +0100

    Update src/

--- src/SemEvalDataParser.java
+++ src/SemEvalDataParser.java
@@ -12,6 +12,13 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import edu.cmu.lti.jawjaw.pobj.POS;
+import edu.cmu.lti.lexical_db.ILexicalDatabase;
+import edu.cmu.lti.lexical_db.NictWordNet;
+import edu.cmu.lti.lexical_db.data.Concept;
+import edu.cmu.lti.ws4j.Relatedness;
+import edu.cmu.lti.ws4j.RelatednessCalculator;
+import edu.cmu.lti.ws4j.impl.Lin;
 import edu.stanford.nlp.simple.Document;
 import edu.stanford.nlp.simple.Sentence;
 import it.uniroma1.lcl.babelfy.commons.BabelfyConstraints;
@@ -169,8 +176,8 @@ public class SemEvalDataParser {
 		String curSent = "";
 		String curPart = "";
 		
-		int limit = 100;
-		int start = 1011;
+		int limit = 1100;
+		int start = 252;
 		int num = 1;
 		
 		BabelNet bn = BabelNet.getInstance();
@@ -240,6 +247,7 @@ public class SemEvalDataParser {
 						List<String> words = new ArrayList<String>();
 						List<String> offsets = new ArrayList<String>();
 						List<String> wordnetSynsets = new ArrayList<String>();
+						List<String> babelTypes = new ArrayList<String>();
 						List<String> babelSynsets = new ArrayList<String>();
 						List<String> babelNetURLs = new ArrayList<String>();
 						List<String> DBPediaURLs = new ArrayList<String>();
@@ -255,13 +263,14 @@ public class SemEvalDataParser {
 						    babelNetURLs.add(annotation.getBabelNetURL());
 						    DBPediaURLs.add(annotation.getDBpediaURL());
 						    
-//						    BabelSynset by = bn.getSynset(new BabelSynsetID(annotation.getBabelSynsetID()));
-//						    Set<String> wnSynSet = new HashSet<String>();
-//						    List<WordNetSynsetID> wnSynList = by.getWordNetOffsets();
-//						    for (WordNetSynsetID syn : wnSynList) {
-//						    	wnSynSet.add(syn.toString());
-//						    }					    
-//						    wordnetSynsets.add(wnSynSet.toString());
+						    BabelSynset by = bn.getSynset(new BabelSynsetID(annotation.getBabelSynsetID()));
+						    babelTypes.add(by.getSynsetType().toString());
+						    Set<String> wnSynSet = new HashSet<String>();
+						    List<WordNetSynsetID> wnSynList = by.getWordNetOffsets();
+						    for (WordNetSynsetID syn : wnSynList) {
+						    	wnSynSet.add(syn.toString());
+						    }					    
+						    wordnetSynsets.add(wnSynSet.toString());
 						}
 						
 						String lastOffset = "";
@@ -272,7 +281,8 @@ public class SemEvalDataParser {
 								bw.write(words.get(n)
 					        			+ "\t" + offsets.get(n)
 					        			+ "\t" + babelSynsets.get(n)
-//					        			+ "\t" + wordnetSynsets.get(n)
+					        			+ "\t" + babelTypes.get(n)
+					        			+ "\t" + wordnetSynsets.get(n)
 					        			+ "\t" + babelNetURLs.get(n)
 					        			+ "\t" + DBPediaURLs.get(n)
 					        			+ "\n"
@@ -282,9 +292,10 @@ public class SemEvalDataParser {
 							} else if (n == 0 && (n+1) < words.size()) {		//first concept
 								if (!isIncludedInOffset(offsets.get(n), offsets.get(n+1))) {
 				        			bw.write(words.get(n)
-						        			+ "\t" + offsets.get(n)
+				        					+ "\t" + offsets.get(n)
 						        			+ "\t" + babelSynsets.get(n)
-//						        			+ "\t" + wordnetSynsets.get(n)
+						        			+ "\t" + babelTypes.get(n)
+						        			+ "\t" + wordnetSynsets.get(n)
 						        			+ "\t" + babelNetURLs.get(n)
 						        			+ "\t" + DBPediaURLs.get(n)
 						        			+ "\n"
@@ -297,9 +308,10 @@ public class SemEvalDataParser {
 										|| lastOffset.equals("")
 											) {
 				        			bw.write(words.get(n)
-						        			+ "\t" + offsets.get(n)
+				        					+ "\t" + offsets.get(n)
 						        			+ "\t" + babelSynsets.get(n)
-//						        			+ "\t" + wordnetSynsets.get(n)
+						        			+ "\t" + babelTypes.get(n)
+						        			+ "\t" + wordnetSynsets.get(n)
 						        			+ "\t" + babelNetURLs.get(n)
 						        			+ "\t" + DBPediaURLs.get(n)
 						        			+ "\n"
@@ -316,9 +328,10 @@ public class SemEvalDataParser {
 												)
 										) {
 				        			bw.write(words.get(n)
-						        			+ "\t" + offsets.get(n)
+				        					+ "\t" + offsets.get(n)
 						        			+ "\t" + babelSynsets.get(n)
-//						        			+ "\t" + wordnetSynsets.get(n)
+						        			+ "\t" + babelTypes.get(n)
+						        			+ "\t" + wordnetSynsets.get(n)
 						        			+ "\t" + babelNetURLs.get(n)
 						        			+ "\t" + DBPediaURLs.get(n)
 						        			+ "\n"
@@ -339,6 +352,348 @@ public class SemEvalDataParser {
 		}
 	}
 	
+	public void writeBabelOutputOnlyTitle(String filepath) throws IOException, InvalidBabelSynsetIDException {
+		BufferedReader br = new BufferedReader(new FileReader(filepath));
+		String line, filename = "";
+		StringBuilder sentences = new StringBuilder();
+		
+		line = br.readLine();
+		String sentStr = "";
+		String curSent = "";
+		String curPart = "";
+		
+		int limit = 5;
+		int start = 981;
+		int num = 1;
+		
+		BabelNet bn = BabelNet.getInstance();
+		BabelfyConstraints constraints = new BabelfyConstraints();
+//		SemanticAnnotation a = new SemanticAnnotation(new TokenOffsetFragment(0, 0), "bn:03083790n",
+//		    "http://dbpedia.org/resource/BabelNet", Source.OTHER);
+//		constraints.addAnnotatedFragments(a);
+		BabelfyParameters bp = new BabelfyParameters();
+		bp.setAnnotationResource(SemanticAnnotationResource.BN);
+		bp.setMCS(MCS.ON_WITH_STOPWORDS);
+//		bp.setScoredCandidates(ScoredCandidates.ALL);
+		
+		Babelfy bfy = new Babelfy(bp);
+		
+		while (line != null) {
+			if (!line.startsWith("#begin") && !line.startsWith("#end")) {
+				String[] cols = line.split("\t");
+				if (curSent.equals("")
+						&& !curSent.equals(cols[0].split("\\.")[0] + "." + cols[0].split("\\.")[1])) {
+					
+					curSent = cols[0].split("\\.")[0] + "." + cols[0].split("\\.")[1];
+					
+				} else if (!curSent.equals("")
+						&& !curSent.equals(cols[0].split("\\.")[0] + "." + cols[0].split("\\.")[1])) {
+					curSent = cols[0].split("\\.")[0] + "." + cols[0].split("\\.")[1];
+				} 
+				
+//				if (cols[1].equals("NEWLINE"))	sentStr += "\n";
+//				else if (cols[2].equals("DCT")) sentStr += cols[1];
+//				else if (!cols[2].equals(curPart)) sentStr += "\n\n" + cols[1] + " ";
+//				else sentStr += cols[1] + " ";
+				
+				if (cols[2].equals("TITLE")) sentStr += cols[1] + " ";
+				
+				curPart = cols[2];
+				
+			} else {
+				if (line.startsWith("#begin")) {
+					if (num > (start + limit - 1)) {
+						break;
+					}
+					filename = line.split(" ")[2].substring(1);
+					filename = filename.substring(0, filename.length()-2);
+					sentences.setLength(0);
+				
+				} else if (line.startsWith("#end")) {
+//					for (String l : sentStr.split("\n")) {
+//						
+//						if (!l.trim().equals("")) {
+//							Document doc = new Document(l);
+//							for (Sentence sss : doc.sentences()) {
+////								List<String> words = sss.words();
+//								sentences.append(sss.text() + "\n");
+//							}
+//						}
+//					}
+					
+					if (num >= start) {
+					
+						BufferedWriter bw;
+//						bw = new BufferedWriter(new FileWriter("./data/babel/s1/files/" + filename + ".txt"));
+//				        bw.write(sentStr);
+//				        bw.close();
+						
+						sentStr = sentStr.trim();
+						
+						System.out.println("====" + num + "====");
+						System.out.println(filename + "\t" + sentStr.trim());
+						
+						List<SemanticAnnotation> bfyAnnotations = bfy.babelfy(sentStr, Language.EN, constraints);
+						//bfyAnnotations is the result of Babelfy.babelfy() call
+						List<String> words = new ArrayList<String>();
+						List<String> offsets = new ArrayList<String>();
+						List<String> wordnetSynsets = new ArrayList<String>();
+						List<String> babelSynsets = new ArrayList<String>();
+						List<String> babelNetURLs = new ArrayList<String>();
+						List<String> DBPediaURLs = new ArrayList<String>();
+						
+						for (SemanticAnnotation annotation : bfyAnnotations)
+						{
+						    //splitting the input text using the CharOffsetFragment start and end anchors
+						    String frag = sentStr.substring(annotation.getCharOffsetFragment().getStart(),
+						        annotation.getCharOffsetFragment().getEnd() + 1);
+						    words.add(frag);
+						    offsets.add(annotation.getCharOffsetFragment().getStart() + "," + (annotation.getCharOffsetFragment().getEnd() + 1));
+						    babelSynsets.add(annotation.getBabelSynsetID());
+						    babelNetURLs.add(annotation.getBabelNetURL());
+						    DBPediaURLs.add(annotation.getDBpediaURL());
+						    
+						    BabelSynset by = bn.getSynset(new BabelSynsetID(annotation.getBabelSynsetID()));
+						    Set<String> wnSynSet = new HashSet<String>();
+						    List<WordNetSynsetID> wnSynList = by.getWordNetOffsets();
+						    for (WordNetSynsetID syn : wnSynList) {
+						    	wnSynSet.add(syn.toString());
+						    }					    
+						    wordnetSynsets.add(wnSynSet.toString());
+						}
+						
+						String lastOffset = "";
+						
+						bw = new BufferedWriter(new FileWriter("./data/babel/s1/title/" + filename + ".tsv"));
+						for (int n = 0; n < words.size(); n ++) {
+							if (n == 0 && n == (words.size()-1)) {	//the only concept
+								bw.write(words.get(n)
+					        			+ "\t" + offsets.get(n)
+					        			+ "\t" + babelSynsets.get(n)
+					        			+ "\t" + wordnetSynsets.get(n)
+					        			+ "\t" + babelNetURLs.get(n)
+					        			+ "\t" + DBPediaURLs.get(n)
+					        			+ "\n"
+					        			);
+			        			lastOffset = offsets.get(n);
+								
+							} else if (n == 0 && (n+1) < words.size()) {		//first concept
+								if (!isIncludedInOffset(offsets.get(n), offsets.get(n+1))) {
+				        			bw.write(words.get(n)
+						        			+ "\t" + offsets.get(n)
+						        			+ "\t" + babelSynsets.get(n)
+						        			+ "\t" + wordnetSynsets.get(n)
+						        			+ "\t" + babelNetURLs.get(n)
+						        			+ "\t" + DBPediaURLs.get(n)
+						        			+ "\n"
+						        			);
+				        			lastOffset = offsets.get(n);
+				        		}
+								
+							} else if (n == (words.size()-1) && (n-1) >= 0) {	//last concept
+								if (((!lastOffset.equals("") && !isIncludedInOffset(offsets.get(n), lastOffset)))
+										|| lastOffset.equals("")
+											) {
+				        			bw.write(words.get(n)
+						        			+ "\t" + offsets.get(n)
+						        			+ "\t" + babelSynsets.get(n)
+						        			+ "\t" + wordnetSynsets.get(n)
+						        			+ "\t" + babelNetURLs.get(n)
+						        			+ "\t" + DBPediaURLs.get(n)
+						        			+ "\n"
+						        			);
+				        			lastOffset = offsets.get(n);
+				        		}
+								
+							} else {	//excluding the first concept and the last concept
+								
+								if (!isIncludedInOffset(offsets.get(n), offsets.get(n+1))
+										&& (
+											((!lastOffset.equals("") && !isIncludedInOffset(offsets.get(n), lastOffset)))
+											|| lastOffset.equals("")
+												)
+										) {
+				        			bw.write(words.get(n)
+						        			+ "\t" + offsets.get(n)
+						        			+ "\t" + babelSynsets.get(n)
+						        			+ "\t" + wordnetSynsets.get(n)
+						        			+ "\t" + babelNetURLs.get(n)
+						        			+ "\t" + DBPediaURLs.get(n)
+						        			+ "\n"
+						        			);
+				        			lastOffset = offsets.get(n);
+				        		}
+				        	} 
+						}
+						bw.close();
+					}
+					
+					num ++;
+				}
+				curSent = "";
+				sentStr = "";
+			}
+			line = br.readLine();
+		}
+	}
+	
+	public void getTopicsFromTitle(String filepath) throws IOException, InvalidBabelSynsetIDException {
+		BufferedReader br = new BufferedReader(new FileReader(filepath));
+		String line, filename = "";
+		StringBuilder sentences = new StringBuilder();
+		
+		line = br.readLine();
+		String sentStr = "";
+		String curSent = "";
+		String curPart = "";
+		String dctStr = "";
+		
+		int limit = 1100;
+		int start = 1;
+		int num = 1;
+		
+		BufferedWriter bw = new BufferedWriter(new FileWriter("./data/babel/s1/topic_per_title.tsv"));
+		
+		while (line != null) {
+			if (!line.startsWith("#begin") && !line.startsWith("#end")) {
+				String[] cols = line.split("\t");
+				if (curSent.equals("")
+						&& !curSent.equals(cols[0].split("\\.")[0] + "." + cols[0].split("\\.")[1])) {
+					
+					curSent = cols[0].split("\\.")[0] + "." + cols[0].split("\\.")[1];
+					
+				} else if (!curSent.equals("")
+						&& !curSent.equals(cols[0].split("\\.")[0] + "." + cols[0].split("\\.")[1])) {
+					curSent = cols[0].split("\\.")[0] + "." + cols[0].split("\\.")[1];
+				} 
+				
+//				if (cols[1].equals("NEWLINE"))	sentStr += "\n";
+//				else if (cols[2].equals("DCT")) sentStr += cols[1];
+//				else if (!cols[2].equals(curPart)) sentStr += "\n\n" + cols[1] + " ";
+//				else sentStr += cols[1] + " ";
+				
+				if (cols[2].equals("TITLE")) sentStr += cols[1] + " ";
+				else if (cols[2].equals("DCT")) dctStr = cols[1];
+				
+				curPart = cols[2];
+				
+			} else {
+				if (line.startsWith("#begin")) {
+					if (num > (start + limit - 1)) {
+						break;
+					}
+					filename = line.split(" ")[2].substring(1);
+					filename = filename.substring(0, filename.length()-2);
+					sentences.setLength(0);
+				
+				} else if (line.startsWith("#end")) {
+//					for (String l : sentStr.split("\n")) {
+//						
+//						if (!l.trim().equals("")) {
+//							Document doc = new Document(l);
+//							for (Sentence sss : doc.sentences()) {
+////								List<String> words = sss.words();
+//								sentences.append(sss.text() + "\n");
+//							}
+//						}
+//					}
+					
+					if (num >= start) {
+					
+						sentStr = sentStr.trim();
+						
+//						System.out.println("====" + num + "====");
+						
+						BufferedReader brr = new BufferedReader(new FileReader("./data/babel/s1/title/" + filename + ".tsv"));
+						String linee = brr.readLine();
+						
+						boolean injuring = false, killing = false;
+						
+						String vInjure = "00069879-v";
+						String nInjury = "14285662-n";
+						String aInjured = "01317954-a";
+						
+						String vKill = "01323958-v";
+						String vDie = "00358431-v";
+						String nKilling = "00219012-n";
+						String aDead = "00095280-a";
+						String aDeadly = "00993667-a";
+						String rFatally = "00506577-r";
+						
+						double threshold = 4.0;
+			
+						while (linee != null) {
+							
+							double killingRelScore = 0.0;
+							double injuringRelScore = 0.0;
+							
+							String[] cols = linee.split("\t");
+							String wnSyn = cols[3];
+							wnSyn = wnSyn.substring(1, wnSyn.length()-1);	// remove []
+							if (!wnSyn.equals("")) {
+								wnSyn = wnSyn.split(",")[0].trim();
+								char wnPos = wnSyn.charAt(wnSyn.length()-1);
+								wnSyn = wnSyn.substring(3, wnSyn.length()-1) + "-" + wnPos;	//remove prefix 'wn:' and add '-' before pos
+								
+								SemanticRelatedness rel, rel2;
+								if (wnPos == 'a') {
+									rel = new SemanticRelatedness(wnSyn, aInjured);
+									injuringRelScore = rel.getHsoScore();
+									
+									rel = new SemanticRelatedness(wnSyn, aDead);
+									rel2 = new SemanticRelatedness(wnSyn, aDeadly);
+									killingRelScore = Math.max(rel.getHsoScore(), rel2.getHsoScore());
+									
+								} else if (wnPos == 'v') {
+									rel = new SemanticRelatedness(wnSyn, vInjure);
+									injuringRelScore = rel.getHsoScore();
+									
+									rel = new SemanticRelatedness(wnSyn, vKill);
+									rel2 = new SemanticRelatedness(wnSyn, vDie);
+									killingRelScore = Math.max(rel.getHsoScore(), rel2.getHsoScore());
+									
+								} else if (wnPos == 'n') {
+									rel = new SemanticRelatedness(wnSyn, nInjury);
+									injuringRelScore = rel.getHsoScore();
+									
+									rel = new SemanticRelatedness(wnSyn, nKilling);
+									killingRelScore = rel.getHsoScore();
+								
+								} else if (wnPos == 'r') {
+//									rel = new SemanticRelatedness(wnSyn, nInjury);
+//									injuringRelScore = rel.getHsoScore();
+									
+									rel = new SemanticRelatedness(wnSyn, rFatally);
+									killingRelScore = rel.getHsoScore();
+								}
+								
+								if (injuringRelScore >= threshold) injuring = true;
+								if (killingRelScore >= threshold) killing = true;
+							}	
+//							System.out.println(cols[0] + "\t" + wnSyn + "\t" + injuringRelScore + "\t" + killingRelScore);	//logging purpose
+									
+							linee = brr.readLine();
+						}
+						
+						brr.close();
+						
+//						System.out.println(filename + "\t" + injuring + "\t" + killing + "\t" + sentStr.trim());
+						bw.write(filename + "\t" + dctStr + "\t" + injuring + "\t" + killing + "\t" + sentStr.trim() + "\n");
+						
+					}
+					
+					num ++;
+				}
+				curSent = "";
+				sentStr = "";
+			}
+			line = br.readLine();
+			
+		}
+		
+		bw.close();
+	}
+	
 	public static void main(String[] args) throws Exception {
 		
 		SemEvalDataParser parser = new SemEvalDataParser();
@@ -350,14 +705,19 @@ public class SemEvalDataParser {
 //		parser.writeSentences("./data/trial_data_final/input/s1/docs.conll");
 		
 		//Parse SemEval CoNLL format into sentence (raw text) format, then run BabelFly on the sentences
-//		parser.writeBabelOutput("./data/trial_data_final/input/s1/docs.conll");
+		parser.writeBabelOutput("./data/trial_data_final/input/s1/docs.conll");
 		
-		BabelNet bn = BabelNet.getInstance();
+//		parser.writeBabelOutputOnlyTitle("./data/trial_data_final/input/s1/docs.conll");
+		
+//		parser.getTopicsFromTitle("./data/trial_data_final/input/s1/docs.conll");
+		
+//		BabelNet bn = BabelNet.getInstance();
 		
 		// Gets a BabelSynset from a concept identifier (Babel synset ID).
-		BabelSynset by = bn.getSynset(new BabelSynsetID("bn:00191195n"));
-		BabelSense bs = by.getMainSense(Language.EN);
-		System.out.println(bs.getSimpleLemma() + "---" + bs.getWordNetOffset());
+//		BabelSynset by = bn.getSynset(new BabelSynsetID("bn:00191195n"));
+//		BabelSense bs = by.getMainSense(Language.EN);
+//		System.out.println(bs.getSimpleLemma() + "---" + bs.getWordNetOffset());
+		
 		
 	}
 
---
+++ src/SemanticRelatedness.java
@@ -0,0 +1,155 @@
+import java.util.List;
+
+import edu.cmu.lti.jawjaw.pobj.POS;
+import edu.cmu.lti.lexical_db.ILexicalDatabase;
+import edu.cmu.lti.lexical_db.NictWordNet;
+import edu.cmu.lti.lexical_db.data.Concept;
+import edu.cmu.lti.ws4j.Relatedness;
+import edu.cmu.lti.ws4j.RelatednessCalculator;
+import edu.cmu.lti.ws4j.impl.Lin;
+import edu.cmu.lti.ws4j.impl.Lesk;
+import edu.cmu.lti.ws4j.impl.HirstStOnge;
+import edu.cmu.lti.ws4j.impl.JiangConrath;
+import edu.cmu.lti.ws4j.impl.LeacockChodorow;
+import edu.cmu.lti.ws4j.impl.Path;
+import edu.cmu.lti.ws4j.impl.Resnik;
+import edu.cmu.lti.ws4j.impl.WuPalmer;
+import edu.cmu.lti.ws4j.impl.Vector;
+import edu.cmu.lti.ws4j.impl.VectorPairs;
+
+public class SemanticRelatedness {
+	
+	private double hsoScore;
+	private double pathScore;
+	private double wupScore;
+	
+	public SemanticRelatedness() {
+		
+	}
+	
+	public SemanticRelatedness(String synOffset1, String synOffset2) {
+		ILexicalDatabase db = new NictWordNet();
+		RelatednessCalculator hso = new HirstStOnge(db);
+		RelatednessCalculator path = new Path(db);
+		RelatednessCalculator wup = new WuPalmer(db);	//might work with threshold 0.5
+		
+		char posCh1 = synOffset1.charAt(synOffset1.length()-1);
+		char posCh2 = synOffset1.charAt(synOffset2.length()-1);
+		
+		POS pos1, pos2;
+		switch(posCh1) {
+			case 'a': pos1 = POS.a;
+			case 'n': pos1 = POS.n;
+			case 'v': pos1 = POS.v;
+			case 'r': pos1 = POS.r;
+			default: pos1 = null;
+		}
+		switch(posCh2) {
+			case 'a': pos2 = POS.a;
+			case 'n': pos2 = POS.n;
+			case 'v': pos2 = POS.v;
+			case 'r': pos2 = POS.r;
+			default: pos2 = null;
+		}
+		
+		Concept concept1 = new Concept(synOffset1, pos1, null, null);
+		Concept concept2 = new Concept(synOffset2, pos2, null, null);
+		
+		Relatedness hsoRel = hso.calcRelatednessOfSynset(concept1, concept2);
+		Relatedness pathRel = path.calcRelatednessOfSynset(concept1, concept2);
+		Relatedness wupRel = wup.calcRelatednessOfSynset(concept1, concept2);
+		
+		this.setHsoScore(hsoRel.getScore());
+		this.setPathScore(pathRel.getScore());
+		this.setWupScore(wupRel.getScore());
+	}
+	
+	public static void main(String[] args) throws Exception {
+		
+		ILexicalDatabase db = new NictWordNet();
+//		RelatednessCalculator rc = new HirstStOnge(db);
+//		RelatednessCalculator rc = new Path(db);
+		RelatednessCalculator rc = new WuPalmer(db);	//might work with threshold 0.5
+		
+		List<Concept> test = (List<Concept>) db.getAllConcepts("deadly", "a");
+		System.out.println(test);
+		
+		List<Concept> test2 = (List<Concept>) db.getAllConcepts("fatal", "a");
+		System.out.println(test2);
+		
+		Concept deadly = new Concept("00993667-a", POS.a, null, null);
+		Concept fatal = new Concept("00993529-a", POS.a, null, null);
+		Concept dead = new Concept("00095280-a", POS.a, null, null);
+		
+//		Concept deadly = test.get(0);
+//		Concept fatal = test2.get(0);
+		
+		Concept shoot = new Concept("01137138-v", POS.v, null, null);
+		Concept hit = new Concept("01405044-v", POS.v, null, null);
+		Concept injure = new Concept("00069879-v", POS.v, null, null);
+		Concept investigate = new Concept("00789138-v", POS.v, null, null);
+		
+		Concept news = new Concept("06681177-n", POS.n, null, null);
+		Concept injury = new Concept("14285662-n", POS.n, null, null);
+		Concept death = new Concept("07355491-n", POS.n, null, null);
+		Concept killing = new Concept("00219012-n", POS.n, null, null);
+		Concept murder = new Concept("00220522-n", POS.n, null, null);
+		
+		Concept deadlyAdv = new Concept("00304898-r", POS.r, null, null);
+		Concept fatally = new Concept("00506577-r", POS.r, null, null);
+		
+		Relatedness rel = rc.calcRelatednessOfSynset(fatal, deadly);
+		System.out.println(rel.getScore());
+		rel = rc.calcRelatednessOfSynset(fatal, dead);
+		System.out.println(rel.getScore());
+		rel = rc.calcRelatednessOfSynset(shoot, injure);
+		System.out.println(rel.getScore());
+		rel = rc.calcRelatednessOfSynset(hit, injure);
+		System.out.println(rel.getScore());
+		rel = rc.calcRelatednessOfSynset(investigate, injure);
+		System.out.println(rel.getScore());
+		rel = rc.calcRelatednessOfSynset(news, injury);
+		System.out.println(rel.getScore());
+		rel = rc.calcRelatednessOfSynset(injury, injure);
+		System.out.println(rel.getScore());
+		rel = rc.calcRelatednessOfSynset(fatal, killing);
+		System.out.println(rel.getScore());
+		rel = rc.calcRelatednessOfSynset(killing, death);
+		System.out.println(rel.getScore());
+		rel = rc.calcRelatednessOfSynset(murder, killing);
+		System.out.println(rel.getScore());
+		
+		rel = rc.calcRelatednessOfSynset(fatally, fatal);
+		System.out.println(rel.getScore());
+		
+		System.out.println("---");
+		SemanticRelatedness semrel = new SemanticRelatedness("01405044-v", "00069879-v");
+		System.out.println(semrel.getWupScore());
+		
+	}
+
+	public double getHsoScore() {
+		return hsoScore;
+	}
+
+	public void setHsoScore(double hsoScore) {
+		this.hsoScore = hsoScore;
+	}
+
+	public double getPathScore() {
+		return pathScore;
+	}
+
+	public void setPathScore(double pathScore) {
+		this.pathScore = pathScore;
+	}
+
+	public double getWupScore() {
+		return wupScore;
+	}
+
+	public void setWupScore(double wupScore) {
+		this.wupScore = wupScore;
+	}
+
+}
